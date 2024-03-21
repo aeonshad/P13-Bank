dadefc1c8a787351f335fe83edeb13e0098377a0
aeonshad <shadaeon@gmail.com>
2024-03-21 17:00:32 +0100

    Added routing configuration using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Home from './pages/home';
+import SignIn from './pages/sign-in';
+import User from './pages/user';
 import './styles/main.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
-    return <Home />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/user" element={<User />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
