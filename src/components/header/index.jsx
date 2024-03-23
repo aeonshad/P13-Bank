a0b6961ffda4e69c37c8ec31ba912cb20e452a71
aeonshad <shadaeon@gmail.com>
2024-03-23 16:51:50 +0100

    Added responsive navigation menu with burger icon

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom';
 import Logout from '../logout';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 function Header() {
     const auth = useSelector((state) => state.auth);
     const user = useSelector((state) => state.user);
+    const [burgerOpen, setBurgerOpen] = useState(false);
+
+    const toggleBurger = () => {
+        setBurgerOpen(!burgerOpen);
+    };
+
     return (
         <nav className="main-nav">
             <a className="main-nav-logo" href="/">
@@ -12,7 +19,7 @@ function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </a>
             {auth.token ? (
-                <div>
+                <div className="main-nav-content" id={burgerOpen ? 'active' : ''}>
                     <Link to="/user" className="main-nav-item">
                         <i className="fa-solid fa-user-circle"></i>
                         {user.firstName}
@@ -20,13 +27,16 @@ function Header() {
                     <Logout />
                 </div>
             ) : (
-                <div>
+                <div className="main-nav-content" id={burgerOpen ? 'active' : ''}>
                     <Link to="/signin" className="main-nav-item">
                         <i className="fa-solid fa-user-circle"></i>
                         Sign In
                     </Link>
                 </div>
             )}
+            <div className={burgerOpen ? 'icons-burger-active' : 'icons-burger'} onClick={toggleBurger}>
+                &#9776;
+            </div>
         </nav>
     );
 }
